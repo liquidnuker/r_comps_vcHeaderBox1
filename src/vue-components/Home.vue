ff27234735057dad01ba54e16d808b1d9acdd0ae
administrator <liquidnuker@users.noreply.github.com>
2017-09-26 10:35:30 -0700

    preparing vcheaderbox1...

--- src/vue-components/Home.vue
+++ src/vue-components/Home.vue
@@ -1,13 +1,25 @@
 <template>
   <div>
-    Home.vue
+  <!-- vcHeaderBox101 -->
+  <h2 class="item-id">vcHeaderBox101</h2>
+    <div class="row">
+      <div class="col-sm-3">39</div>
+      <div class="col-sm-9">
+        <vcHeaderBox101 />
+      </div>
+    </div>
+
   </div>
 </template>
 <script>
+const vcHeaderBox101 = () => import ('./vcHeaderBox101.vue');
   export default {
     data () {
       return {    
       }
+    },
+    components: {
+      vcHeaderBox101: vcHeaderBox101
     },
     mounted: function () {
       console.log("Home.vue mounted");
---
+++ src/vue-components/vcHeaderBox101.vue
@@ -0,0 +1,35 @@
+<template>
+<div>
+  <!-- begin headerbox101 -->
+  <header class="row headerbox101">
+    <h2 tabindex="0">ZZZZ</h2>
+    <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod
+    tempor incididunt ut labore et dolore magna aliqua.</p>
+    <hr>
+  </header>
+  <!-- end headerbox101 -->
+</div>
+</template>
+<script>
+export default {
+  data () {
+    return {   
+
+    }
+  },
+  watch: {
+  // : function () {
+  // }
+  },
+  props: [
+    "prName1"
+  ],
+  components: {
+  },
+  mounted: function () {
+    
+  },
+  methods: {        
+  }
+}
+</script>
